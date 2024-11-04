dc60642fb6fad854bcac7a3910d01e30994c528c
Hafız <ramizovhafiz@gmail.com>
2024-11-04 22:33:27 +0300

    Initial Commit

---
+++ IosTutorial/IosTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosTutorialApp.swift
+//  IosTutorial
+//
+//  Created by Nejat on 4.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct IosTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
